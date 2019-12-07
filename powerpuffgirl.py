d2fd89241e18001fec086a6d100ff17cacb3367c
chrisrandolph <57924306+chrisrandolph@users.noreply.github.com>
2019-12-07 00:47:56 -0500

    Add files via upload

---
+++ powerpuffgirl.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  7 00:08:15 2019
+
+@author: crand
+"""
+
+class Powerpuffgirl:
+    def init (self, name, ingredient, ability):
+       self.name = name
+       self.ingredient = ingredient
+       self.ability = ability
+
+
+
+
+
+Powerpuffgirl1 = Powerpuffgirl("Blossom", "Everything Nice", "Ice Breath")
+Powerpuffgirl2 = Powerpuffgirl("Bubbles", "Sugar", "Supersonic Waves")
+Powerpuffgirl3 = Powerpuffgirl("Buttercup", "Spice", "Fights Hard")
+
+
+
